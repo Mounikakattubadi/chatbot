6761bc0b401daf69e1881e70b3b52eec8d1ffb38
Mounika <mounikakattubadi94@gmail.com>
2026-02-13 16:57:18 +0530

    backend port changes

--- my-chatbot/src/components/ChatApp.vue
+++ my-chatbot/src/components/ChatApp.vue
@@ -19,7 +19,7 @@ const messages = ref<Message[]>([])
 
 async function sendToBackend(message: string) {
   try {
-    const res = await fetch("http://127.0.0.1:5000/chat", {
+    const res = await fetch("http://127.0.0.1:8000/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message })
